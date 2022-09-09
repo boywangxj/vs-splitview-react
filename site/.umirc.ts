d481589f4858521fb1b9d4a1e4619c60cc383a10
boywangxj <boywangxj@163.com>
2022-09-09 22:05:31 +0800

    修复ghpages 路径问题

--- site/.umirc.ts
+++ site/.umirc.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'vs-split-view',
   mode: 'site',
+  base: '/vs-splitview-react',
+  publicPath: '/vs-splitview-react/',
+  history: { type: 'hash' },
   // more config: https://d.umijs.org/config
 });
